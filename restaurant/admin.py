313a4875eb6e8200ba92d9656cb2b996a56c9fee
Igor Mundim <mundim.github@gmail.com>
2022-06-01 14:43:55 -0300

    add ORM

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import MenuItem, Restaurant
+
+
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(MenuItem)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'price']
+
+
+admin.site.register(Restaurant, CategoryAdmin)
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=65)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    category = models.CharField(max_length=65)
+    name = models.CharField(max_length=65)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(
+        upload_to='restaurant/covers/%Y/%m/%d/', blank=True, default='')
+
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
